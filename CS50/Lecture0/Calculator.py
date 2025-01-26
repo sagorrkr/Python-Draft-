1a2ec1643549773a2cea31b5088c9abf918ada68
sagorrkr <ridoykum9@gmail.com>
2025-01-26 16:58:39 +0800

    functions

---
+++ CS50/Lecture0/Calculator.py
@@ -0,0 +1,8 @@
+def main():
+    x = int(input("what's x? "))
+    print("X squared is", square(x))
+
+def square(n):
+    return n * n
+
+main()
---
+++ CS50/Lecture0/Function.py
@@ -0,0 +1,27 @@
+def main():
+    #Ask for users name
+    name = input("what's your name? ").strip().title()
+    Hello(name)
+
+def Hello(to = "World!"):
+    print("Hello, ", to)
+
+main()
+
+
+
+
+
+
+
+
+"""def Hello(to = "World!"):
+    print("Hello, ", to)
+
+Hello()
+
+#Ask for users name
+name = input("what's your name? ").strip().title()
+Hello(name)"""
+
+
