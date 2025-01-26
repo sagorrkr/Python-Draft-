ab846c11b055d1c85b30970290ea434b1aba8a22
sagorrkr <ridoykum9@gmail.com>
2025-01-26 14:18:18 +0800

    Lecture 0

---
+++ Lecture0.py
@@ -0,0 +1,9 @@
+print("Hello, World!")
+
+Name = input("What's your name? ")
+
+print("Hello", Name)
+
+Age = int(input("How old are you? "))
+
+print("So, you are going to be", str(Age + 1) ,"next year")
