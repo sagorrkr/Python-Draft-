4dbc7afb47e62deeec7f4b9854f13bbd7846eeb7
sagorrkr <ridoykum9@gmail.com>
2025-01-26 16:10:51 +0800

    Added int

---
+++ CS50/Lecture0/IntegersAndOperators.py
@@ -0,0 +1,18 @@
+x = 6
+y = 2
+
+a = x + y  
+b = x - y
+c = x * y
+d = x / y
+e = x // y
+f = x % y
+
+print(a,b,c,d,e,f)
+
+m = int(input("input the first number: "))
+n = int(input("input the second number: "))
+
+print(m+n)
+
+print(int(input("input the third number: "))+ int(input("input the forth number: ")))
