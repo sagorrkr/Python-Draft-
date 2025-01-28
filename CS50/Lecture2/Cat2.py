45f261e349247223daee552730d46d380e78ce58
sagorrkr <ridoykum9@gmail.com>
2025-01-28 13:32:36 +0800

    Lecture 2

---
+++ CS50/Lecture2/Cat2.py
@@ -0,0 +1,18 @@
+
+def main():
+    meow(3)
+
+def meow(n):
+    for _ in range(n):
+        print("meow")
+    
+main()
+
+
+
+while True:
+    n = int(input("Whats's N? "))
+    if n > 0:
+        break
+for i in range(n):
+    print("meow")
