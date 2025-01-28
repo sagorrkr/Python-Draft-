c8246c36fc725e1cde296097086144233c7f7a47
sagorrkr <ridoykum9@gmail.com>
2025-01-28 13:26:21 +0800

    Lecture 2

---
+++ CS50/Lecture2/Cat.py
@@ -0,0 +1,16 @@
+# start -> meow -> meow -> meow stop
+i = 3
+
+while i != 0:
+    print("meow")
+    i -= 1 
+
+
+for n in [0,1,2]:
+    print("Woff")
+    n += 1
+
+for x in range(3):
+    print("Bark")
+
+print("meaw \n" * 3, end = "")
