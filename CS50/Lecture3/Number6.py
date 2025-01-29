8e4651ac27609e9b44351e8a63adffa2ae6cc450
sagorrkr <ridoykum9@gmail.com>
2025-01-29 15:47:28 +0800

    Cleaning it using 'pass'

---
+++ CS50/Lecture3/Number6.py
@@ -0,0 +1,12 @@
+def main():
+    x = get_int()
+    print(f"X is {x}")
+
+def get_int():
+    while True:
+        try:
+            return int(input("What's X? "))
+        except ValueError:
+            pass
+
+main()
