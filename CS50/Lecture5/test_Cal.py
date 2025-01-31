56755c8c1785577920d444f6334f771fbed632f6
sagorrkr <ridoykum9@gmail.com>
2025-01-31 12:18:15 +0800

    Pytest

--- CS50/Lecture5/test_Cal.py
+++ CS50/Lecture5/test_Cal.py
@@ -5,13 +5,17 @@ def main():
 
 def test_square():
     try:
-        assert square(2) == 4
+        assert square(-2) == 4
     except AssertionError:
-        print("2 squared was not 4")
+        print("-2 squared was not 4")
     try:
-        assert square(3) == 9
+        assert square(-3) == 9
     except AssertionError:
-        print("2 squared was not 4")
+        print("-2 squared was not 4")
+    try:
+        assert square(0) == 0
+    except AssertionError:
+        print(" squared was not 0")
 
 if __name__ == "__main__":
     main()
