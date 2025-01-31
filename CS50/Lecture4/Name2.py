c138273da186fd7b5a3e59c0678e63ef7a16865b
sagorrkr <ridoykum9@gmail.com>
2025-01-31 11:19:53 +0800

    updated

--- CS50/Lecture4/Name2.py
+++ CS50/Lecture4/Name2.py
@@ -3,5 +3,5 @@ import sys
 if len(sys.argv) < 2:
     sys.exit("Too few arguments")
 
-for arg in sys.argv:
+for arg in sys.argv[1:]: #to indicate from second to the last index, we might use [1:-1]
     print("Hello, My name is ", arg)
