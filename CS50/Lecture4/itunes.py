fc73dbbfdf67c333802064c827d702db39bd90dc
sagorrkr <ridoykum9@gmail.com>
2025-01-31 11:19:19 +0800

    updated

--- CS50/Lecture4/itunes.py
+++ CS50/Lecture4/itunes.py
@@ -4,10 +4,10 @@ import json
 import requests
 import sys
 
-"""if len(sys.argv) != 2:
-  sys.exit()"""
+if len(sys.argv) != 2:
+  sys.exit()
 
-response = requests.get("https://itunes.apple.com/search?entity=%20song&limit=20&term=" + "weezer") #sys.argv[1]
+response = requests.get("https://itunes.apple.com/search?entity=%20song&limit=20&term=" + sys.argv[1]) 
 
 #print(json.dumps(response.json(), indent = 2))
 
