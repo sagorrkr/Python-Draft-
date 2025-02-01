af20796c3129e42c4b285a2073d86b047f953fea
sagorrkr <ridoykum9@gmail.com>
2025-02-01 14:00:59 +0800

    Reading a file

--- CS50/Lecture6/names2.py
+++ CS50/Lecture6/names2.py
@@ -1,5 +1,5 @@
 name = input("What's your name? ")
 
-file = open("names.txt", "w")
-file.write(name)
+file = open("names.txt", "a")   # "w" for writing , "a" for appending
+file.write(f"{name}\n")  #file.write(name)
 file.close()
