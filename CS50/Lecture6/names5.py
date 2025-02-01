d726f9ff0f679315ea91d4cd77befa546977da5e
sagorrkr <ridoykum9@gmail.com>
2025-02-01 14:07:17 +0800

    shorting files

---
+++ CS50/Lecture6/names5.py
@@ -0,0 +1,13 @@
+with open("names.txt", "r") as file:
+    for line in file:
+        print("Hello", line.rstrip())
+
+
+names = []
+
+with open("names.txt") as file:
+    for line in file:
+        names.append(line.rstrip())
+
+for name in sorted(names):
+    print(f"Hello, {name}")
