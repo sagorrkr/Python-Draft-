1e5bdf21a4413d795e18d943fa860b1659261ac3
sagorrkr <ridoykum9@gmail.com>
2025-02-05 15:11:50 +0800

    updated

--- CS50/Lecture7/twitter3.py
+++ CS50/Lecture7/twitter3.py
@@ -2,6 +2,6 @@ import re
 
 url = input("URL: ").strip()
 # := or-else operator
-if  matches := re.search(r"^https?://?(?:www\.)?twitter\.com/(.+)$", url, re.IGNORECASE): 
+if  matches := re.search(r"^https?://?(?:www\.)?twitter\.com/([/w]+)", url, re.IGNORECASE): #?: is to escape()
     print(f"Username: ", matches.group(1))
 
