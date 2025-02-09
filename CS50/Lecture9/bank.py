e338f181127d7e5b18fd5b480cb9bdc620c939f4
sagorrkr <ridoykum9@gmail.com>
2025-02-09 14:46:31 +0800

    Global variable

--- CS50/Lecture9/bank.py
+++ CS50/Lecture9/bank.py
@@ -3,6 +3,19 @@ balance = 0
 
 def main():
     print("Banalce:", balance)
+    deposite(100)
+    withdraw(50)
+    print("Banalce:", balance)
+
+
+def deposite(n):
+    global balance
+    balance += n
+
+def withdraw(n):
+    global balance
+    balance -= n
+    
 
 
 
