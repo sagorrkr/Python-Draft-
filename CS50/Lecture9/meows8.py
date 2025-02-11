27b45937f3377d7277da5c23c6e1d644dcd3d21a
sagorrkr <ridoykum9@gmail.com>
2025-02-11 19:50:44 +0800

    added default value and type in arguments

--- CS50/Lecture9/meows8.py
+++ CS50/Lecture9/meows8.py
@@ -4,5 +4,5 @@ parser = argparse.ArgumentParser(description = "Meow like a cat")
 parser.add_argument("-n",default=1, help = "number of times to meow", type = int)  #python3 meows7.py -h 
 args = parser.parse_args()
 
-for _ in range(int(args.n)):
+for _ in range(args.n):
     print("meow")
