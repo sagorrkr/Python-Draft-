759565431fe4afb0dd929db216d52b3939521223
sagorrkr <ridoykum9@gmail.com>
2025-02-11 19:39:29 +0800

    argparse

--- CS50/Lecture9/meows7.py
+++ CS50/Lecture9/meows7.py
@@ -1,7 +1,7 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-n")
+parser.add_argument("-n", help = "number of times to meow")  #python3 meows7.py -h 
 args = parser.parse_args()
 
 for _ in range(int(args.n)):
