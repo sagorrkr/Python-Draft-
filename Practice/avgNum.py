c75eae6d14573bcba1a1c91a76a0a581004598f4
sagorrkr <ridoykum9@gmail.com>
2025-02-17 16:34:53 +0800

    finding avg number_

--- Practice/avgNum.py
+++ Practice/avgNum.py
@@ -1,3 +1,3 @@
-num = list(map(int, input("Enter numbers: ").split()))
+num = list(map(int, input("Enter numbers separated by space: ").split()))
 #avg = sum(num)/len(num)
 print(sum(num)/len(num)) #print(avg)
