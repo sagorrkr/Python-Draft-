2ae751bd28f62ae1981f36732a49dd2f543b84da
sagorrkr <ridoykum9@gmail.com>
2025-02-18 14:50:30 +0800

    Finding even and odd numbers

---
+++ Practice/CorePythonBasics/Even_Odd.py
@@ -0,0 +1,8 @@
+#Write a program to check if a number is even or odd.
+
+num = int(input("Enter a number to evaluate: "))
+
+if num % 2 == 0:
+    print("The number is Even!")
+else: 
+    print("The number is Odd!")
