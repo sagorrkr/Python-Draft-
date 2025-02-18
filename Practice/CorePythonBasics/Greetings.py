56313a1b7f98b3b1b132b6070d4f90f610af33af
sagorrkr <ridoykum9@gmail.com>
2025-02-18 14:51:15 +0800

    Formatting string

---
+++ Practice/CorePythonBasics/Greetings.py
@@ -0,0 +1,8 @@
+#Create a program that takes a user's name and age as input and 
+#prints a message like: "Hello [Name], you are [Age] years old.
+
+
+name =  input("Welcome! What's your name? ")
+age = int(input("How old are you? "))
+
+print(f"Hello {name}, you are {age} years old.")
