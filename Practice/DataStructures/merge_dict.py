fd0d866c5f9c595b42cfe968946ef41bac8b6210
sagorrkr <ridoykum9@gmail.com>
2025-02-20 17:46:07 +0800

    Merging two dictionaries

---
+++ Practice/DataStructures/merge_dict.py
@@ -0,0 +1,15 @@
+#Create a program that merges two dictionaries into one.
+
+def merge_dict(dict1,dict2):
+    
+    merged_dict = dict1.copy()
+
+    merged_dict.update(dict2)
+    return merged_dict
+
+dict1 = {"a": 1, "b": 2, "c": 3}
+dict2 = {"d": 4, "e": 5, "f": 6}
+
+result = merge_dict(dict1, dict2)
+
+print(result)
