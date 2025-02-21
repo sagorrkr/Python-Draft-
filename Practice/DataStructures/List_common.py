9a5856cdc99a7d8086e16cb972ccc59a7d79e78c
sagorrkr <ridoykum9@gmail.com>
2025-02-21 12:07:05 +0800

    Findng common element in list using for loop

---
+++ Practice/DataStructures/List_common.py
@@ -0,0 +1,10 @@
+def common_elem(lst1, lst2):
+    return[element for element in lst1 if element in lst2]
+
+list1 = [ 1, 2, 3, 4, 5]
+list2 = [ 3, 4, 5, 6, 7]
+
+
+result = common_elem(list1, list2)
+
+print(result)
