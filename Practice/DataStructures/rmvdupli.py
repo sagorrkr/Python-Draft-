ee1af342703aa5c754543da44e76644f0d07450d
sagorrkr <ridoykum9@gmail.com>
2025-02-22 14:38:46 +0800

    removing duplicates while preserving the order of elements

---
+++ Practice/DataStructures/rmvdupli.py
@@ -0,0 +1,18 @@
+#Write a program to remove duplicates from a list using sets.
+
+def removedup(inputlist):
+
+    seen = set()
+    uniqueList = []
+
+    for item in inputlist:
+        if item not in seen:
+            uniqueList.append(item)
+            seen.add(item)
+
+    return uniqueList
+
+inputlist = [1, 2, 3, 5, 6, 5, 4, 2, 7, 5, 9]
+
+result = removedup(inputlist)
+print(result)
