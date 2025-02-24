9bea252184f34ee16324b8526c8cf0086b381d47
sagorrkr <ridoykum9@gmail.com>
2025-02-24 20:32:15 +0800

    writing and reading from the file

--- Practice/FileAndErrorHandling/readnwrite.py
+++ Practice/FileAndErrorHandling/readnwrite.py
@@ -1,3 +1,5 @@
+#Create a program that writes user input to a file and then reads the file to display the content.
+
 def writeTofile(filepath):
     
     userinput = input("Write something to save in the file: ")
@@ -7,3 +9,16 @@ def writeTofile(filepath):
         file.write(userinput)
 
         print("The file saved successfully!")
+def readFromfile(filepath):
+
+    with open(filepath, 'r') as file:
+        try:
+            content = file.read()
+            print(f"content of the file {content}")
+
+        except FileNotFoundError:
+            print("File not found, Check the filepath again!")
+
+filepath = "user_input.txt"
+writeTofile(filepath)
+readFromfile(filepath)
