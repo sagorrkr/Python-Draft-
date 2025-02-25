bf19ba3c3bd47ec06d7e67f95f5926292f870ec0
sagorrkr <ridoykum9@gmail.com>
2025-02-25 09:10:36 +0800

    appending to a file without deleting the existing content

---
+++ Practice/FileAndErrorHandling/writetofile.py
@@ -0,0 +1,12 @@
+#Writing on a local file
+
+def writingOnfile(filepath):
+    userInput = input("Enter some text to save in the file: ")
+
+    with open(filepath, 'a') as file:
+        file.write(userInput + '\n')
+
+    print("Text saved successfully!")
+filepath = "user_input.txt"
+content = writingOnfile(filepath)
+
