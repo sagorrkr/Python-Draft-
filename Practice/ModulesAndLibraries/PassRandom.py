de8643088fddc0724bd3059e8f23424238936ab8
sagorrkr <ridoykum9@gmail.com>
2025-03-01 17:50:06 +0800

    Random pass generator

--- Practice/ModulesAndLibraries/PassRandom.py
+++ Practice/ModulesAndLibraries/PassRandom.py
@@ -1,1 +1,20 @@
 #Generating random password by taking the number of digit from user
+import random
+import string
+
+def generate_password(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password  = ''.join(random.choices(characters, k = length))
+    return password
+
+try:
+    length = int(input("Enter the length of the password: "))
+    if length <= 0:
+        print("Password length must be a positive integer.")
+    else:
+        password = generate_password(length)
+        print(f"Generated password: {password}")
+
+except ValueError:
+    print("Error: Please enter a valid integer.")
+
