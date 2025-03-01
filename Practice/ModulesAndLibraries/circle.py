60743148fbcc6685846751a870c515f8e89d543c
sagorrkr <ridoykum9@gmail.com>
2025-03-01 20:32:29 +0800

    Using math, finding the area of a circle

---
+++ Practice/ModulesAndLibraries/circle.py
@@ -0,0 +1,15 @@
+import math
+
+def AreaCircle(radius):
+    area = math.pi * (radius ** 2)
+    return area
+
+try: 
+    radius = int(input("Enter the radius of the circle: "))
+    if radius <= 0:
+        print("Radius can't be negative.")
+    else:
+        circle = AreaCircle(radius)
+        print(f"The are of the circle is {circle}")
+except ValueError:
+    print("Error: Valid valie: int Only")
