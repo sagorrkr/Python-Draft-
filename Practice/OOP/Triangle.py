4ed2913801f6922396da9d82b698a3a320f25088
sagorrkr <ridoykum9@gmail.com>
2025-03-08 18:05:08 +0800

    Triangle class for cheching if its valid

--- Practice/OOP/Triangle.py
+++ Practice/OOP/Triangle.py
@@ -12,6 +12,7 @@ class Triangle:
         return self.side1 + self.side2 + self.side3
     
     def is_valid_triangle(self):
+        # A triangle is valid if the sum of any two sides is greater than the third side
         return (self.side1 + self.side2 > self.side3) and \
                (self.side2 + self.side3 > self.side1) and \
                (self.side1 + self.side3 > self.side2)
