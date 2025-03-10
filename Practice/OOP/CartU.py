2845c9960d2bb4977aa6afbc53592c16903a28b6
sagorrkr <ridoykum9@gmail.com>
2025-03-10 14:12:56 +0800

    Updated

--- Practice/OOP/CartU.py
+++ Practice/OOP/CartU.py
@@ -23,16 +23,37 @@ class ShoppingCart:
         return totalcost
     
     def displayDetails(self):
+        if self.items:
+            print(f"Items in the cart {self.cart_id}")
+            for item in self.items:
+                print(f"-{item[0]} : {item[1]: .2f}")
+            print(f"Total Cost: {self.calculateTotalcost(): .2f}")
+        else:
+            print(f"Cart {self.cart_id}")
     
 def createShopingCart():
+    print("\nEnter Shopping Details: ")
+    cart_id = input("Enter cart id: ")
+    cart = ShoppingCart(cart_id)
+
+    while True:
+        item_name = input("Enter item name(or Done to finish)")
+        if item_name.lower() == "done":
+            break
+        try:
+            price = float(input(f"Enter the price for {item_name} $:"))
+            cart.AddItems(item_name,price)
+        except ValueError:
+            print("Error: Invalid price.")
+    return cart
 
 if __name__ == "__main__":
-    cart = ShoppingCart(cart_id = "C69")
+    cart = createShopingCart()
 
-    cart.AddItems("Apple", 2.0)
-    cart.AddItems("Banana", 1.5)
-    cart.AddItems("Orange", 3.0)
-
+    cart.displayDetails()
     cart.RemoveItem("Banana")
 
-    print(f"Total cost: {cart.calculateTotalcost(): .2f}")
+    removeitem = input("Enter the name of the item to remove: ")
+
+    cart.RemoveItem(removeitem)
+    cart.displayDetails()
