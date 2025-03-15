edbad2ad4394a6e7540ee6c0af34a75052a6be82
sagorrkr <ridoykum9@gmail.com>
2025-03-15 22:08:33 +0800

    Bank account class and bank method to manupulate

--- Practice/AdvancedOOP/BankAccount.py
+++ Practice/AdvancedOOP/BankAccount.py
@@ -10,7 +10,7 @@ class BankAccount:
     def deposit(self, amount):
         if amount > 0:
             self.balance += amount
-            print(f"Deposited {amount} into {self.account_number}. Current balance: ${self.balance: .2f}")
+            print(f"Deposited {amount} into {self.account_number}. Current balance: ${self.balance:.2f}")
         else:
             print("Deposit amount must be more than 0. ")
     
@@ -18,7 +18,7 @@ class BankAccount:
         if amount > 0:
             if amount <= self.balance:
                 self.balance -= amount
-                print(f"Withdrawal {amount} from {self.account_number}. Current balance: ${self.balance: .2f}")
+                print(f"Withdrew {amount} from {self.account_number}. Current balance: ${self.balance:.2f}")
             else:
                 print(f"Insufficient funds in {self.account_number}")
         else:
@@ -28,8 +28,8 @@ class Bank:
     def __init__(self):
         self.accounts =  {}
 
-    def add_accounts(self, account):
-        self.accounts[account.account.account_number] = account
+    def add_account(self, account):
+        self.accounts[account.account_number] = account
         print(f"Added account {account.account_number} for {account.account_holder}")
 
     def display_all_accounts(self):
@@ -38,7 +38,36 @@ class Bank:
             for account_number, account in self.accounts.items():
                 print(f"Account Number {account_number}, Holder: {account.account_holder}")
         else:
-            print("No accounts in bank. ")
+            print("No accounts in the bank. ")
+
+    def display_all_balances(self):
+        if self.accounts:
+            print("\nAll account balances: ")
+            for account_number, account in self.accounts.items():
+                print(f" Account: {account_number} \nBalance: {account.balance:.2f}")
+
+        else:
+            print("No account in the bank. ")
+
+if __name__ == "__main__":
+    bank = Bank()
+
+    account1 = BankAccount(account_number = 1234, account_holder = "Roy", balance = 1000)
+    account2 = BankAccount(account_number = 4567, account_holder = "Rogas", balance = 3000)
+    account3 = BankAccount(account_number = 7890, account_holder = "Ridoy", balance = 6969)
+
+    bank.add_account(account1)
+    bank.add_account(account2)
+    bank.add_account(account3)
+
+    bank.display_all_accounts()
+
+    account1.deposit(3000)
+    account2.withdraw(500)
+    account3.withdraw(3969)
+
+    bank.display_all_balances()
+
 
         
 
