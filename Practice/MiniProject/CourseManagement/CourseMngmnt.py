79c9f6a4975d129ed642ecf5b02ae6edd2e87972
sagorrkr <ridoykum9@gmail.com>
2025-03-19 16:18:17 +0800

    Course management with Course and student class

---
+++ Practice/MiniProject/CourseManagement/CourseMngmnt.py
@@ -0,0 +1,24 @@
+class Course:
+    def __init__(self,course_name, course_code):
+        self.course_name = course_name
+        self.course_code = course_code
+
+    def __str__(self):
+        return f"{self.course_code}: {self.course_name}"
+
+class Student:
+    def __init__(self,student_id, name):
+        self.student_id = student_id
+        self.name = name
+        self.enrolled_courses = []
+
+    def enroll(self, course):
+        if course not in self.enrolled_courses:
+            self.enrolled_courses.append(course)
+            print(f"{self.name} has enrolled in {course.course_name}")
+        else:
+            print(f"{self.name} has already enrolled in this course.")
+
+    
+
+    
