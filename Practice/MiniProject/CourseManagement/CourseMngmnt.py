fcc256aee1031782e465985de323a53554a66f8d
sagorrkr <ridoykum9@gmail.com>
2025-03-20 10:24:42 +0800

    Course management using 2 class

--- Practice/MiniProject/CourseManagement/CourseMngmnt.py
+++ Practice/MiniProject/CourseManagement/CourseMngmnt.py
@@ -20,11 +20,25 @@ class Student:
             print(f"{self.name} has already enrolled in this course.")
     def display_enrolled_courses(self):
         if self.enrolled_courses:
-            print(f"\n{self.name} Enrolled courses")   
+            print(f"\n{self.name}'s enrolled courses:")   
             for course in self.enrolled_courses:
                 print(course)    
         else:
             print(f"{self.name} is not enrolled in any course")
-    
+
+if __name__ == "__main__":
+    course1 = Course(course_name = "Maths", course_code = 1234)
+    course2 = Course(course_name = "Physics", course_code = 2345)
+    course3 = Course(course_name = "Science", course_code = 3456)
+    course4 = Course(course_name = "Java", course_code = 4567)
+
+student1 = Student(student_id = 20240513001, name = "Mishu")
+student2 = Student(student_id = 20240513003, name = "Vane")
+student3 = Student(student_id = 20240513001, name = "Ridoy")
+
+student1.enroll(course1)
+student1.enroll(course2)
+
+student1.display_enrolled_courses() 
 
     
