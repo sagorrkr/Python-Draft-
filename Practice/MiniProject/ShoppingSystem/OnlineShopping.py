325766df77431d5edd0b65a2895fc9952b33dce0
sagorrkr <ridoykum9@gmail.com>
2025-03-23 21:39:44 +0800

    Online shopping with multiple class

--- Practice/MiniProject/ShoppingSystem/OnlineShopping.py
+++ Practice/MiniProject/ShoppingSystem/OnlineShopping.py
@@ -8,3 +8,21 @@ class Product:
     def __str__(self):
         return f"{self.name} (Id: {self.product_id}) | - {self.price:.2f} (Stock: {self.stock})"
     
+    def reduce_stock(self, quantity):
+        if self.stock >= quantity:
+            self.stock -= quantity
+            return True
+        else:
+            return False
+        
+class Customer:
+    def __init__(self, customer_id, name, email):
+        self.customer_id = customer_id
+        self.name = name
+        self.email = email
+        self.cari = ()
+        self.order_history = []
+
+    def __str__(self):
+        return f"Customer {self.customer_id}: {self.name} , {self.email}"
+    
